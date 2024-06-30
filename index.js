a043880bf823e59b1a388bab754655686ab9edea
Junhui Zhang <junhuizhang.g@gmail.com>
2024-07-01 01:29:16 +1200

    Add environment variable on Render

--- index.js
+++ index.js
@@ -234,7 +234,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const APP_PORT = process.env.APP_PORT
-app.listen(APP_PORT, () => {
-    console.log(`Server running on port ${APP_PORT}`)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
