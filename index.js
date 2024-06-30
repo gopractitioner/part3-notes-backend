f8a58da6e68e951de0d2343ebda00e2d63b3616e
Junhui Zhang <junhuizhang.g@gmail.com>
2024-07-01 01:15:56 +1200

    new env setting

--- index.js
+++ index.js
@@ -234,7 +234,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const APP_PORT = process.env.PORT
+const APP_PORT = process.env.APP_PORT
 app.listen(APP_PORT, () => {
     console.log(`Server running on port ${APP_PORT}`)
 })
