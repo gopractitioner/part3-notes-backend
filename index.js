00ee8db1dae3e536b8ca9ec22626a0c661bc2996
Junhui Zhang <junhuizhang.g@gmail.com>
2024-07-02 20:43:37 +1200

    update

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
 
 app.use(express.json());
+app.use(requestLogger)
+
 app.use(cors());
 let notes = [
     {
